8469ae9ed9b337160fd8ece747c3a4a854d0afdb
ychong032 <yowlimchong2403@gmail.com>
2025-02-04 22:14:45 +0800

    Add nav menu for small screens

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 import { incrementVisitorCount } from "./visitorCount.js";
 
-let updatedVisitorCount = await incrementVisitorCount();
-const visitorCountElement = document.getElementById("visitor-counter");
+const menuButton = document.getElementById("menu-button");
+const menu = document.getElementById("menu");
+const menuLinks = menu.children;
+const closeButton = document.getElementById("close-button");
+const modal = document.getElementById("modal");
 
-let digits = updatedVisitorCount.toString().split("");
-for (let digit of digits) {
-    visitorCountElement.innerHTML += `<span class=\"bg-sky-300 rounded-md p-2 font-mono\">${digit}</span>`;
+menuButton.addEventListener("click", toggleMenuDisplay);
+closeButton.addEventListener("click", toggleMenuDisplay);
+for (let link of menuLinks) {
+    link.addEventListener("click", toggleMenuDisplay);
+}
+modal.addEventListener("touchstart", toggleMenuDisplay);
+populateVisitorCount();
+
+async function populateVisitorCount() {
+    let updatedVisitorCount = await incrementVisitorCount();
+    const visitorCountElement = document.getElementById("visitor-counter");
+
+    let digits = updatedVisitorCount.toString().split("");
+    for (let digit of digits) {
+        visitorCountElement.innerHTML += `<span class=\"bg-sky-300 rounded-md p-2 font-mono\">${digit}</span>`;
+    }
+}
+
+function toggleMenuDisplay() {
+    menu.classList.toggle("hidden");
+    menu.classList.toggle("grid");
+    closeButton.classList.toggle("hidden");
+    menuButton.classList.toggle("hidden");
+    modal.classList.toggle("hidden");
 }
