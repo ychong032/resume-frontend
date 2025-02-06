debee220b1d396a48f4b2142ceafa42f3693699e
Chong Yow Lim <69382702+ychong032@users.noreply.github.com>
2025-02-06 23:05:25 +0800

    Implement dark mode and favicon (#9)
    * Add dark mode
    
    * Fix icons for dark mode
    
    * Improve dark mode buttons
    
    * Add favicon

--- src/index.js
+++ src/index.js
@@ -5,13 +5,14 @@ const menu = document.getElementById("menu");
 const menuLinks = menu.children;
 const closeButton = document.getElementById("close-button");
 const modal = document.getElementById("modal");
+const lightModeButton = document.getElementById("light-mode-button");
+const lightModeButtonMd = document.getElementById("light-mode-button-md");
+const darkModeButton = document.getElementById("dark-mode-button");
+const darkModeButtonMd = document.getElementById("dark-mode-button-md");
+const htmlElement = document.documentElement;
 
-menuButton.addEventListener("click", toggleMenuDisplay);
-closeButton.addEventListener("click", toggleMenuDisplay);
-for (let link of menuLinks) {
-    link.addEventListener("click", toggleMenuDisplay);
-}
-modal.addEventListener("touchstart", toggleMenuDisplay);
+initDarkModeButtons();
+initEventListeners();
 populateVisitorCount();
 
 async function populateVisitorCount() {
@@ -20,8 +21,45 @@ async function populateVisitorCount() {
 
     let digits = updatedVisitorCount.toString().split("");
     for (let digit of digits) {
-        visitorCountElement.innerHTML += `<span class=\"bg-sky-300 rounded-md p-2 font-mono\">${digit}</span>`;
+        visitorCountElement.innerHTML += `<span class=\"bg-green-500 dark:bg-green-400 rounded-md p-2 font-mono\">${digit}</span>`;
     }
+}
+
+function initDarkModeButtons() {
+    lightModeButtonMd.classList.toggle(
+        "hidden",
+        localStorage.theme === "dark" ||
+            (!("theme" in localStorage) &&
+                window.matchMedia("(prefers-color-scheme: dark)").matches)
+    );
+    darkModeButtonMd.classList.toggle(
+        "hidden",
+        !lightModeButtonMd.classList.contains("hidden")
+    );
+
+    lightModeButton.classList.toggle(
+        "hidden",
+        localStorage.theme === "dark" ||
+            (!("theme" in localStorage) &&
+                window.matchMedia("(prefers-color-scheme: dark)").matches)
+    );
+    darkModeButton.classList.toggle(
+        "hidden",
+        !lightModeButton.classList.contains("hidden")
+    );
+}
+
+function initEventListeners() {
+    menuButton.addEventListener("click", toggleMenuDisplay);
+    closeButton.addEventListener("click", toggleMenuDisplay);
+    for (let link of menuLinks) {
+        link.addEventListener("click", toggleMenuDisplay);
+    }
+    modal.addEventListener("touchstart", toggleMenuDisplay);
+    lightModeButton.addEventListener("click", toggleDarkMode);
+    lightModeButtonMd.addEventListener("click", toggleDarkMode);
+    darkModeButton.addEventListener("click", toggleDarkMode);
+    darkModeButtonMd.addEventListener("click", toggleDarkMode);
 }
 
 function toggleMenuDisplay() {
@@ -30,4 +68,16 @@ function toggleMenuDisplay() {
     closeButton.classList.toggle("hidden");
     menuButton.classList.toggle("hidden");
     modal.classList.toggle("hidden");
+}
+
+function toggleDarkMode() {
+    localStorage.setItem(
+        "theme",
+        htmlElement.classList.contains("dark") ? "light" : "dark"
+    );
+    htmlElement.classList.toggle("dark");
+    lightModeButtonMd.classList.toggle("hidden");
+    darkModeButtonMd.classList.toggle("hidden");
+    lightModeButton.classList.toggle("hidden");
+    darkModeButton.classList.toggle("hidden");
 }
